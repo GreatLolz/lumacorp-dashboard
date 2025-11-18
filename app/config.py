052403ca284a23a83318f29b4176c691986ca007
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-11-18 16:18:22 +0100

    add character and corp id to config

--- app/config.py
+++ app/config.py
@@ -7,6 +7,8 @@ class Settings(BaseSettings):
     eve_client_secret: str
     eve_callback_url: str = "http://localhost:8000/callback"
 
+    esi_client_character_id: str
+    esi_client_corp_id: str
     esi_client_useragent: str = "lumacorp-api/0.1.0"
     
     token_file_path: str = "./data/tokens.json"
