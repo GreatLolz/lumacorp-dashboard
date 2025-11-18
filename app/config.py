ff961e8bcc12b96587a15b6a68c4a7ffead1e794
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-11-18 15:59:04 +0100

    create app config

---
+++ app/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+load_dotenv()
+
+class Settings(BaseSettings):
+    eve_client_id: str
+    eve_client_secret: str
+    eve_callback_url: str = "http://localhost:8000/callback"
+    
+    token_file_path: str = "./data/tokens.json"
+    
+    class Config:
+        case_sensitive = False
+
+settings = Settings()
