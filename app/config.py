780370731bb20c6b3b96b07846993b23a112fa6a
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-11-18 16:07:47 +0100

    add useragent field to config

--- app/config.py
+++ app/config.py
@@ -6,6 +6,8 @@ class Settings(BaseSettings):
     eve_client_id: str
     eve_client_secret: str
     eve_callback_url: str = "http://localhost:8000/callback"
+
+    esi_client_useragent: str = "lumacorp-api/0.1.0"
     
     token_file_path: str = "./data/tokens.json"
     
