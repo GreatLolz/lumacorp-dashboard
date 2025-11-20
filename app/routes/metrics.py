eda840de8b659ffbb663145f3a230e115911f195
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-11-20 01:00:09 +0100

    add wallet metrics to prometheus

--- app/routes/metrics.py
+++ app/routes/metrics.py
@@ -3,11 +3,16 @@ from prometheus_client import CONTENT_TYPE_LATEST, Gauge, generate_latest
 from fastapi import Response
 from fastapi import APIRouter
 
-wallet_balance = Gauge("wallet_balance", "Wallet balance")
+from app.wallet import get_wallet_balance
+
+wallet_balance_gauge = Gauge("wallet_balance", "Wallet balance per division", ["division"])
 
 router = APIRouter(prefix="/metrics")
 
 @router.get("/")
 def metrics():
-    wallet_balance.set(random.randint(0, 1000000))
+    balances = get_wallet_balance()
+    for name, balance in balances.items():
+        wallet_balance_gauge.labels(division=name).set(balance)
+
     return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
