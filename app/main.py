d55fd6dc3f44a50b2e7f37f1561ccdb017bf1548
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-11-22 15:08:12 +0100

    run initial fetch on app startup

--- app/main.py
+++ app/main.py
@@ -1,8 +1,33 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.db import engine, Base
-from app.models.token import RefreshToken
+from app.market import get_profit_indexes
+import asyncio
 
-app = FastAPI()
+async def load_data():
+    try:
+        while True:
+            try:
+                await get_profit_indexes(refresh=True)
+            except Exception as e:
+                print(f"Error loading profit data: {e}")
+            for _ in range(24 * 60 * 60 // 10):
+                try:
+                    await asyncio.sleep(10)
+                except asyncio.CancelledError:
+                    print("Background task cancelled")
+                    return
+    except asyncio.CancelledError:
+        print("Background task cancelled")
+        return
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    task = asyncio.create_task(load_data())
+    yield
+    task.cancel()
+
+app = FastAPI(lifespan=lifespan)
 
 Base.metadata.create_all(bind=engine)
 
